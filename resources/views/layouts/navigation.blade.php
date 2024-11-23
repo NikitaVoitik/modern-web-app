135a722ad9d82f123eb215912581ddf905c69a05
NikitaVoitik <nikitavoitik2006@gmail.com>
2024-11-23 15:25:37 +0100

    Log Out bug fix

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -60,7 +60,7 @@
                                 @csrf
 
                                 <x-dropdown-link :href="route('logout')"
-                                                 onclick="Event.preventDefault();
+                                                 onclick="event.preventDefault();
                                                  this.closest('form').submit();">
                                     {{ __('Log Out') }}
                                 </x-dropdown-link>
