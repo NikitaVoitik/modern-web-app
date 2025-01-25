fbe8b2f6c40bf14cea5691f0748fea575a6513e2
NikitaVoitik <nikitavoitik2006@gmail.com>
2025-01-25 16:11:47 +0100

    scheduled backups

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// Schedule the backup command
+Artisan::command('schedule:backup', function () {
+    $schedule = app(Schedule::class);
+    $schedule->command('backup:run')->daily()->at('02:00');
+})->purpose('Schedule daily backups');
