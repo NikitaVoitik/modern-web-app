5d1717c477c5e3e4ee90f9da67f3981753e71852
NikitaVoitik <nikitavoitik2006@gmail.com>
2025-01-29 20:11:46 +0100

    BackUp Fix

--- routes/console.php
+++ routes/console.php
@@ -4,10 +4,5 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
-
-Artisan::command('backup:run', function ($user) {
-    // ...
-})->purpose('BackUp')->schedule()->daily();
+Schedule::command('backup:clean')->daily()->at('01:00');
+Schedule::command('backup:run')->daily()->at('01:30');
